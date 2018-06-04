558c4d7cd07edd6b6126324808cb1477c6b09a21
Timmy <luili16@126.com>
2018-06-04 11:29:36 +0800

    temp commit

--- todoappdagger/src/main/java/com/llx278/todoappdagger/tasks/TasksActivity.java
+++ todoappdagger/src/main/java/com/llx278/todoappdagger/tasks/TasksActivity.java
@@ -1,10 +1,21 @@
 package com.llx278.todoappdagger.tasks;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.llx278.todoappdagger.R;
 
 public class TasksActivity extends AppCompatActivity {
 
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+
+
+    }
 }
 
 
